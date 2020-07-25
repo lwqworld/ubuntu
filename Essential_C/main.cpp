edbcad3ee15b3011e866f5a4dea2850d4a0b920f
lei <eternaldreamstar@outlook.com>
2020-07-25 18:36:30 +0800

    1.6 vector 存储整数

--- Essential_C/main.cpp
+++ Essential_C/main.cpp
@@ -3,37 +3,30 @@
  * @Github: https://github.com/lwqworld
  * @Date: 2020-07-25 17:16:06
  * @LastEditors: Beihuang
- * @LastEditTime: 2020-07-25 18:06:12
+ * @LastEditTime: 2020-07-25 18:32:16
  * @FilePath: /Cpp_pj/Essential_C/main.cpp
- */ 
-#include<iostream>
-#include<string>
-#include<iomanip>
-#include<cstring>
+ */
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    const int nm_size=128;
-    char user_name[nm_size];
-    
-    cout<<"Please enter your  name: ";
-    cin>>setw(nm_size)>>user_name;
-    switch(strlen(user_name)){
-        case 0:
-            cout<<"Ah,the user with no name."
-            <<"Well ,ok ,hi,user with no name\n";
-            break;
-        case 1:
-            cout<<"A  1-character name? Hum,have you resd Kafka?: "
-            <<"hello, "<<user_name<<endl;
-            break;
-        case 127:
-            cout<<"That is a very  big name: indeed -- "<<"we may have needed to shorten it!\n"
-            <<"In any case,\n";
-        default:
-            cout<<"Hello, "<<user_name<<" -- happy to make ypur acquaintance!\n";
-            break;
-        
+int main()
+{
+    vector<int> ivec;
+    int ival;
+    while (cin >> ival)
+    {
+        ivec.push_back(ival);
     }
+    int sum, ix;
+    for (sum = 0, ix = 0; ix < ivec.size(); ++ix)
+    {
+        sum += ivec[ix];
+        cout <<ivec[ix] << endl;
+    }
+    int average = sum / ivec.size();
+    cout<<"sum of "<<ivec.size()
+    <<" elements: "<<sum
+    <<" . Average: "<<average<<endl;
     return 0;
 }
